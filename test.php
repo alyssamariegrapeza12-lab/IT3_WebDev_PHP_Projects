fe040c78fbaf10f1902a725a006b6ebb5f8c3f81
Alyssa Marie Grapeza <alyssamariegrapeza12@gmail.com>
2026-01-28 09:53:00 +0800

    Setup: Environment verification with test.php

---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+echo"<h1>Environment Setup Successfull</h1>";
+echo"<h1>yael solo leveling</h1>";
+echo"<h1>yael cultivation</h1>";
+echo"<h1>yael godsss</h1>";
+echo"<h1>blood xxxx</h1>";
+phpinfo();
+?>
